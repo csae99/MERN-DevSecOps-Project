2d45fe1cfaba745bc44fda9557af3668540df797
csae99 <146985056+csae99@users.noreply.github.com>
2025-06-30 18:49:26 +0530

    Update main.tf

--- Terraform /modules /ec2-instance/main.tf
+++ Terraform /modules /ec2-instance/main.tf
@@ -28,6 +28,13 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "jenkins  or argocd "
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
