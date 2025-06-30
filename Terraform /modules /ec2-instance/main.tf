f3ee625e04e52544e785eaf0b9c5429cda4dddbd
csae99 <146985056+csae99@users.noreply.github.com>
2025-06-30 18:57:29 +0530

    Update main.tf

--- Terraform /modules /ec2-instance/main.tf
+++ Terraform /modules /ec2-instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = var.security_group_ids
   key_name               = var.key_name
   user_data              = var.user_data
 
@@ -13,33 +13,5 @@ resource "aws_instance" "this" {
 
   tags = {
     Name = var.instance_name
-  }
-}
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.instance_name}-sg"
-  description = "Allow SSH and app ports"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "jenkins  or argocd "
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
