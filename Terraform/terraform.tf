e49fcc2171908ba7bd6ea4033792762d17f30e6d
csae99 <146985056+csae99@users.noreply.github.com>
2025-07-07 12:25:00 +0530

    Update terraform.tf

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.40.0"  # Use a stable 5.x release
+      source = "hashicorp/aws"
+      version = "6.2.0"
     }
   }
 }
