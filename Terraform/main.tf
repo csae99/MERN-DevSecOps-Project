2d2fe98649ada3fb5c5dac5d92d56efd0642bdf6
csae99 <146985056+csae99@users.noreply.github.com>
2025-07-07 13:06:03 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,63 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Security group for Jenkins master
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Allow SSH and Jenkins UI"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Security group for agent node
-resource "aws_security_group" "agent_sg" {
-  name        = "agent-sg"
-  description = "Allow SSH and K8s tools"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
 module "vpc" {
   source              = "./modules/vpc"
   vpc_cidr            = "10.0.0.0/16"
