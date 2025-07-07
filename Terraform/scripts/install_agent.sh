6cd378ee08cd749819b9b0823c54ca0992f73f6b
csae99 <146985056+csae99@users.noreply.github.com>
2025-07-07 13:45:52 +0530

    Update install_agent.sh

--- Terraform/scripts/install_agent.sh
+++ Terraform/scripts/install_agent.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y docker.io curl wget apt-transport-https gnupg2 software-
 # Enable Docker
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $USER && newgrp docker
 
 # Install kind (Kubernetes in Docker)
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
@@ -29,22 +29,22 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 # Create Kubernetes cluster with kind
-# kind create cluster --name devsecops-cluster
+curl -LO "https://github.com/csae99/MERN-DevSecOps-Project/blob/master/Terraform/scripts/kind-config.yaml"
+kind create cluster --name devsecops-cluster --config kind-config.yaml
 
 # Install ArgoCD
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Install Prometheus & Grafana using Helm
-# curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo update
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
 
-# kubectl create namespace monitoring
+kubectl create namespace monitoring
+helm install prometheus prometheus-community/prometheus --namespace monitoring
 
-# helm install prometheus prometheus-community/prometheus --namespace monitoring
-
-# helm install grafana prometheus-community/grafana --namespace monitoring --set adminPassword='admin' --set service.type=NodePort
+helm install grafana prometheus-community/grafana --namespace monitoring --set adminPassword='admin' --set service.type=NodePort --set service.nodePort=30000
 
 echo "Agent setup with kind, ArgoCD, Prometheus, and Grafana completed."
